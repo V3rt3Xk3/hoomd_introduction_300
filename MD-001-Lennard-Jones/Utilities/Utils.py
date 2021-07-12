0b185b5e7709200f1a6f22fb733d58cc04ac2d12
Zoltán Balázs <zoltanbalazsacademic@gmail.com>
2021-07-12 20:35:26 +0200

    // A sample MD simulation

--- MD-001-Lennard-Jones/Utilities/Utils.py
+++ MD-001-Lennard-Jones/Utilities/Utils.py
@@ -1,4 +1,23 @@
+import os
+import gsd.hoomd
 
+class GSDUtils:
+
+    @staticmethod
+    def saveSnapshot(fileName, gsdSnapshot, hoomdSnapshot):
+        # WOW: This is superwierd, I couldn't find a good way to copy a hoomd.Snapshot to gsd.hoomd.Snapshot. The only difference seems like, there is a validate method for the properties and the  class itself.
+        gsdSnapshot.particles.N = hoomdSnapshot.particles.N
+        gsdSnapshot.particles.position = hoomdSnapshot.particles.position
+        gsdSnapshot.particles.orientation = hoomdSnapshot.particles.orientation
+        gsdSnapshot.particles.typeid = hoomdSnapshot.particles.typeid
+        gsdSnapshot.particles.types = hoomdSnapshot.particles.types
+        gsdSnapshot.configuration.box = hoomdSnapshot.configuration.box
+
+        # NOTE: Saving the snapshot
+        currentPath = os.path.dirname(__file__)
+        snapshotPath = SysUtils.generateSnaptshotPathFromUtils(currentPath=currentPath, fileName=fileName)
+        with gsd.hoomd.open(name=snapshotPath, mode='xb') as file:
+            file.append(gsdSnapshot)
 
 
 class SysUtils:
@@ -11,5 +30,16 @@ class SysUtils:
             file_path = "./snapshots/" + fileName
         elif (currentPath != ""):
             file_path = currentPath + '/snapshots/' + fileName
+
+        return file_path
+    
+    @staticmethod
+    def generateSnaptshotPathFromUtils(currentPath, fileName):
+        # HACK: Generating the savePath
+
+        if (currentPath == ""):
+            file_path = "./../snapshots/" + fileName
+        elif (currentPath != ""):
+            file_path = currentPath + '/../snapshots/' + fileName
 
         return file_path
--- MD-001-Lennard-Jones/lennard_jones.py
+++ MD-001-Lennard-Jones/lennard_jones.py
@@ -1,5 +1,5 @@
 import os
-from Utilities.Utils import SysUtils
+from Utilities.Utils import SysUtils, GSDUtils
 
 import hoomd
 import hoomd.md
@@ -9,7 +9,6 @@ import math
 import itertools
 import numpy
 import matplotlib
-from numpy.core.fromnumeric import repeat
 
 # NOTE: General Sys setup
 currentPath = os.path.dirname(__file__)
@@ -41,10 +40,14 @@ orientation = [(1,0,0,0)] * numberOfParticles
 snapshot = gsd.hoomd.Snapshot()
 snapshot.particles.N = numberOfParticles
 snapshot.particles.position = position
+
+#Debug
+prositions2Print = snapshot.particles.position
+
 snapshot.particles.orientation = orientation
 # NOTE: Each particle also has a type
 snapshot.particles.typeid = [0] * numberOfParticles
-snapshot.particles.types = ['particleType/Shape ETC - a label']
+snapshot.particles.types = ['A']
 # NOTE: Three box lengths L_x, L_y, L_z, and 3 tilt factors. Set equal box lengths 0 tilt factors to define a cubic box
 snapshot.configuration.box = [L, L, L, 0, 0, 0]
 
@@ -82,6 +85,54 @@ integrator.methods.append(nvt)
 
 # NOTE: Assigning the integrator
 simulation.operations.integrator = integrator
+
+# NOTE: As HoomD velocities are defaulted to 0 we have to give a starting value
+# NOTE: Use the ThermodynamicQuantities class to compute properties of the system
+simulation.state.thermalize_particle_momenta(filter=hoomd.filter.All(), kT=1.5)
+thermodynamic_properties = hoomd.md.compute.ThermodynamicQuantities(
+                                                filter=hoomd.filter.All())
+
+# NOTE: ThermodynamicQuantities is a Compute, an Operation that computes properties of the system state. Some computations can only be performed during or after a simulation run has started.
+simulation.operations.computes.append(thermodynamic_properties)
+simulation.run(0)
+
+# NOTE: Compressing the System
+# NOTE: ######################
+# Setting the ramp
+ramp = hoomd.variant.Ramp(A=0, B=1, t_start=simulation.timestep, t_ramp=20000)
+rho = simulation.state.N_particles / simulation.state.box.volume
+
+# Setting the target density
+initial_box = simulation.state.box
+final_box = hoomd.Box.from_box(initial_box)  # make a copy of initial_box
+final_rho = 1.2
+final_box.volume = simulation.state.snapshot.particles.N / final_rho
+
+# NOTE: To avoid destabilizing the integrator with large forces due to large box size changes, scale the box with a small period.
+box_resize_trigger = hoomd.trigger.Periodic(10)
+box_resize = hoomd.update.BoxResize(box1=initial_box,
+                                    box2=final_box,
+                                    variant=ramp,
+                                    trigger=box_resize_trigger)
+simulation.operations.updaters.append(box_resize)
+
+# NOTE: Running the simulation to resize the box
+simulation.run(20001)
+# # Debug
+# print(simulation.state.box == final_box)
+simulation.operations.updaters.remove(box_resize)
+
+# WOW: This is superwierd, I couldn't find a good way to copy a hoomd.Snapshot to gsd.hoomd.Snapshot. The only difference seems like, there is a validate method for the properties and the  class itself.
+gsdSnapshot = gsd.hoomd.Snapshot()
+hoomdSnapshot = simulation.state.snapshot
+GSDUtils.saveSnapshot("resizedBox.gsd" ,gsdSnapshot, hoomdSnapshot)
+
+# NOTE: Running the actual simulation to resize the box
+simulation.run(5e5)
+# WOW: This is superwierd, I couldn't find a good way to copy a hoomd.Snapshot to gsd.hoomd.Snapshot. The only difference seems like, there is a validate method for the properties and the  class itself.
+gsdSnapshot = gsd.hoomd.Snapshot()
+hoomdSnapshot = simulation.state.snapshot
+GSDUtils.saveSnapshot("SimulationEnd.gsd" ,gsdSnapshot, hoomdSnapshot)
 
 print(simulation.state)
 print(simulation.operations.integrator)
